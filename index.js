991545d411cf7420648f1638adca6fb7d8031e46
ludjan <ludviggjansson@gmail.com>
2022-07-12 15:51:26 +0200

    This was a lot

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 var http = require('http');
 var server = http.Server(app);
 
-app.use(express.static('client'));
+app.use(express.static('public'));
 
 server.listen(PORT, function() {
   console.log('Chat server running');
