c5783329d4a4206096f44993dfb37752b3543ed0
ludjan <ludviggjansson@gmail.com>
2022-07-13 16:50:50 +0200

    test

--- index.js
+++ index.js
@@ -12,3 +12,22 @@ server.listen(PORT, function() {
   console.log('Web server running');
   return true
 });
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT * FROM recipe;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
