e2b6ee2be71a1ad05afe106354ba47b3356fd047
ludjan <ludviggjansson@gmail.com>
2022-07-13 14:03:28 +0200

    a

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var PORT = process.env.PORT || 5000;
-var express = require('express');
+// var express = require('express');
+import express from 'express'
 var app = express();
 
 var http = require('http');
