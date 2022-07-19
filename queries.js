99791f48d32b3a1bd52cb4c009a1d93bf5ae1f2f
ludjan <ludviggjansson@gmail.com>
2022-07-19 18:56:24 +0200

    Update queries.js

--- queries.js
+++ queries.js
@@ -10,6 +10,7 @@ const client = new Client( {
 client.connect()
 
 const getRecipes = (request, response) => {
+    console.log('Getting all recipes')
     client.query(`SELECT * FROM recipe ORDER BY id ASC`, (error, results) => {
         if (error) throw error
         response.status(200).json(results.rows)
