6bec0029ac4ad2352080559b7cfd8fda86b1dcc7
ludjan <ludviggjansson@gmail.com>
2022-07-19 11:35:45 +0200

    Update recipe.js

--- public/recipe.js
+++ public/recipe.js
@@ -1,15 +1,9 @@
 const feed = document.getElementById('recipe-feed')
 
-const url = 'https://lud-recipe-app.herokuapp.com/api/recipes'
+const url='https://lud-recipe-app.herokuapp.com/api/recipes'
+// const url='http://localhost:3000/api/recipes'
 
 fetch(url)
     .then(response => response.json())
-    .then(data => {
-      console.log(data)
-      var recipeLi = ""
-      data.forEach(recipe => {
-        recipeLi += `<h3> ${recipe.name}</h3>`
-      })
-      feed.innerHTML += recipeLi
-    })
+    .then(data => console.log(data))
     .catch(err => console.log(err))
