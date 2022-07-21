a6c416571b8f38196baf8f883554c9ede231d442
ludjan <ludviggjansson@gmail.com>
2022-07-21 15:45:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var port = process.env.PORT || 5001
 var express = require('express')
 const cors = require('cors')
 var http = require('http')
+const { hostname } = require('os')
 
 var app = express() // use the express framework to handle dynamic responses to different pages
 app.use(express.json()) // make sure express parses bodies with json
 app.use(cors()) // make sure we can access the api from the outside
+
+
 
 var server = http.Server(app)
 
