575bee13acdac7d07f5c5fe1aef635f480f6cd27
ludjan <ludviggjansson@gmail.com>
2022-08-01 12:14:52 +0200

    Update scripts.sql

--- db_scripts/scripts.sql
+++ db_scripts/scripts.sql
@@ -75,3 +75,13 @@ INSERT INTO recipe_app.recipeIngredientUnit(recipe_id, ingredient_id, unit_id, q
 (1, 4, 5, 2),
 (1, 5, 2, 2);
 
+CREATE VIEW recipeIngredientSimple (recipe_id, ingredient, quantity, unit) AS 
+    SELECT 
+        riu.recipe_id,
+        i.name AS ingredient_name,
+        riu.quantity AS quantity,
+        u.name AS unit_name 
+    FROM
+        recipe_app.recipeIngredientUnit AS riu
+        JOIN recipe_app.ingredient AS i ON riu.ingredient_id = i.id
+        INNER JOIN recipe_app.unit AS u ON riu.unit_id = u.id;
