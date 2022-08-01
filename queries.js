c6565bcfcc93615ba0bdf99958482e59ab1cf135
ludjan <ludviggjansson@gmail.com>
2022-08-01 11:03:24 +0200

    Update queries.js

--- queries.js
+++ queries.js
@@ -245,7 +245,7 @@ const createFullRecipe = (request, response) => {
 
         for (let i=0; i<ingredients.length; i++) {
             var element = ingredients[i]
-            valuesStr += `(${id}, ${element.ingredientId}, ${element.quantity}, SELECT id FROM recipe_app.unit WHERE name = '${element.unit}') `
+            valuesStr += `(${id}, ${element.ingredientId}, ${element.quantity}, (SELECT id FROM recipe_app.unit WHERE name = '${element.unit}')) `
         }
 
         console.log(valuesStr)
