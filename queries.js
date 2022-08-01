524e30cfce5b143a2e9363f436938d430d2a9faa
ludjan <ludviggjansson@gmail.com>
2022-08-01 22:59:58 +0200

    Update queries.js

--- queries.js
+++ queries.js
@@ -253,7 +253,7 @@ const createFullRecipe = (request, response) => {
 
         // console.log(insertRelationValuesStr)
 
-        // next queryy
+        // next queryyy
         client.query(
             `INSERT INTO recipe_app.recipeIngredientUnit (recipe_id, ingredient_id, unit_id, quantity) VALUES ${insertRelationValuesStr} RETURNING *`,
             (error, results) => {
